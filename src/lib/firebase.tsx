197aa9509d6b9671304e72590b4a234c726005ce
ngworks1909 <nithin.kanduru1908@gmail.com>
2025-01-24 12:12:42 +0530

    change

--- src/lib/firebase.tsx
+++ src/lib/firebase.tsx
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { getStorage } from 'firebase/storage'
 import { initializeApp } from 'firebase/app'
-import {getMessaging} from 'firebase/messaging'
 
 // Firebase configuration using environment variables
 const firebaseConfig = {
@@ -16,4 +15,3 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
-export const publisher = getMessaging(app)
